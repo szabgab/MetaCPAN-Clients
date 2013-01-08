d9c44923d1996916e125c547c9db64efba23dbe8
Gabor Szabo <szabgab@gmail.com>
2013-01-08 12:16:17 +0200

    improve the packaging

--- lib/MetaCPAN/Clients.pm
+++ lib/MetaCPAN/Clients.pm
@@ -2,5 +2,12 @@ package MetaCPAN::Clients;
 use strict;
 use warnings;
 
+=head1 DESCRIPTION
+
+For an explanation see L<Fetching META data from Meta CPAN|http://perl5maven.com/fetching-meta-data-from-meta-cpan>.
+
+=cut
+
+
 1;
 
