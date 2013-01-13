72fad29f936928d157d276e0a7a66bbefaf2380a
Gabor Szabo <szabgab@gmail.com>
2013-01-13 13:50:03 +0200

    add link to second article

--- lib/MetaCPAN/Clients.pm
+++ lib/MetaCPAN/Clients.pm
@@ -5,7 +5,8 @@ use warnings;
 =head1 DESCRIPTION
 
 For an explanation of the metacpan_meta.pl script see L<Fetching META data from Meta CPAN|http://perl5maven.com/fetching-meta-data-from-meta-cpan>.
-For the metacpan_namespace.pl see L<>.
+For the metacpan_namespace.pl see
+L<List all the Perl modules and distributions in a name-space using Meta CPAN|http://perl5maven.com/list-all-the-perl-modules-and-distributions-in-a-namespace-using-meta-cpan>.
 
 =cut
 
