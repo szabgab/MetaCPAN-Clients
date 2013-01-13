5734c456fb399fae5b2383cfb92bf3e4fa070cb0
Gabor Szabo <szabgab@gmail.com>
2013-01-13 13:43:57 +0200

    prepare for 0.02

--- lib/MetaCPAN/Clients.pm
+++ lib/MetaCPAN/Clients.pm
@@ -4,7 +4,8 @@ use warnings;
 
 =head1 DESCRIPTION
 
-For an explanation see L<Fetching META data from Meta CPAN|http://perl5maven.com/fetching-meta-data-from-meta-cpan>.
+For an explanation of the metacpan_meta.pl script see L<Fetching META data from Meta CPAN|http://perl5maven.com/fetching-meta-data-from-meta-cpan>.
+For the metacpan_namespace.pl see L<>.
 
 =cut
 
