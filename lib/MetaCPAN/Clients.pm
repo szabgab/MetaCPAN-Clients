278499f7e1ea89f6046ddc5aa28011884750c9c9
Gabor Szabo <gabor@szabgab.com>
2013-02-05 22:22:28 +0200

    beef up the POD

--- lib/MetaCPAN/Clients.pm
+++ lib/MetaCPAN/Clients.pm
@@ -2,11 +2,41 @@ package MetaCPAN::Clients;
 use strict;
 use warnings;
 
+=head1 NAME
+
+MetaCPAN::Clients - Some useful stand-alone scripts to access MetaCPAN
+
+=head1 SYNOPSIS
+
+Just read the articles and check out L<MetaCPAN::API>.
+
 =head1 DESCRIPTION
 
 For an explanation of the metacpan_meta.pl script see L<Fetching META data from Meta CPAN|http://perl5maven.com/fetching-meta-data-from-meta-cpan>.
+
 For the metacpan_namespace.pl see
 L<List all the Perl modules and distributions in a name-space using Meta CPAN|http://perl5maven.com/list-all-the-perl-modules-and-distributions-in-a-namespace-using-meta-cpan>.
+
+=head1 RESULTS
+
+Some results using these scripts show:
+
+On January 3 2013 we found that
+L<50% of the new CPAN uploads have a repository link|http://blogs.perl.org/users/gabor_szabo/2013/01/50-of-the-new-cpan-uploads-lack-a-repository-link.html>
+
+On december 28, 2012 we found that
+L<17.4% of CPAN uploads have no license in the META files|http://blogs.perl.org/users/gabor_szabo/2012/12/174-of-cpan-uploads-have-no-license-in-the-meta-files.html>
+
+=head1 AUTHOR
+
+L<Gabor Szabo|http://szabgab.com/>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Gabor Szabo.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
