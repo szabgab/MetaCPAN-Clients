072ae64581ec1f2801cc639ad0058125b7061926
Gabor Szabo <gabor@szabgab.com>
2013-09-29 19:53:20 +0300

    add link to release and add --verbose flag

--- bin/metacpan_meta.pl
+++ bin/metacpan_meta.pl
@@ -4,8 +4,14 @@ use warnings;
 use 5.010;
 
 use Data::Dumper;
+use Getopt::Long qw(GetOptions);
 use MetaCPAN::API;
 my $mcpan = MetaCPAN::API->new;
+
+my $verbose = 0;
+GetOptions(
+    'verbose' => \$verbose,
+);
 
 my ($size, $pauseid) = @ARGV;
 die "Usage: $0 N [PAUSEID]  (N = number of most recent distributions)\n" if not $size;
@@ -47,7 +53,8 @@ my %licenses;
 my @missing_license;
 my @missing_repo;
 my %repos;
-my $found = 0;
+my $license_found = 0;
+my $repo_found = 0;
 my $hits = scalar @{ $r->{hits}{hits} };
 foreach my $d (@{ $r->{hits}{hits} }) {
     my $license = $d->{fields}{license};
@@ -56,13 +63,14 @@ foreach my $d (@{ $r->{hits}{hits} }) {
     my $repo    = $d->{fields}{'resources.repository'};
 
     if ($license and $license ne 'unknown' and $license ne 'open_source') {
-        $found++;
+        $license_found++;
         $licenses{$license}++;
     } else {
         push @missing_license, [$distro, $author];
     }
 
     if ($repo and $repo->{url}) {
+        $repo_found++;
         if ($repo->{url} =~ m{http://code.google.com/}) {
             $repos{google}++;
         } elsif ($repo->{url} =~ m{git://github.com/}) {
@@ -92,15 +100,18 @@ foreach my $d (@{ $r->{hits}{hits} }) {
     }
 }
 @missing_license = sort {$a->[0] cmp $b->[0]} @missing_license;
-@missing_repo    = sort {$a->[0] cmp $b->[0]} @missing_repo;
+@missing_repo    = sort {$a->[0] cmp $b->[0]}
+	map { $_->[2] = "http://metacpan.org/release/$_->[0]"; $_ }
+	@missing_repo;
 say "Total asked for: $size";
 say "Total received : $hits";
-say "License found: $found, missing " . scalar(@missing_license);
-say "Repos missing: " . scalar(@missing_repo);
+printf "License found: %3s, missing: %s\n", $license_found, scalar(@missing_license);
+printf "Repos found:   %3s, missing: %s\n", $repo_found, scalar(@missing_repo);
 say "-" x 40;
 print Dumper \%repos;
 print Dumper \%licenses;
-print 'missing_licenses: ' . Dumper \@missing_license;
-print 'missing_repo: ' . Dumper \@missing_repo;
-
+if ($verbose) {
+    print 'missing_licenses: ' . Dumper \@missing_license;
+    print 'missing_repo: ' . Dumper \@missing_repo;
+}
 
